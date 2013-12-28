1d27a75f8cf93d41cdae6acfcb3758b2a00dd575
Jon Doveston <jon@doveston.me.uk>
2013-12-28 21:58:04 +0000

    use accessor

--- lib/haipa/links.rb
+++ lib/haipa/links.rb
@@ -13,9 +13,9 @@ module Haipa
 
     def method_missing(name, *args, &block)
       name = name.to_s
-      if @data.has_key?(name)
-        href = @data[name]['href']
-        if @data[name]['templated'] == true
+      if data.has_key?(name)
+        href = data[name]['href']
+        if data[name]['templated'] == true
           template = Addressable::Template.new(href)
           options = args.last.is_a?(::Hash) ? args.pop : {}
           href = template.expand(options).to_s
